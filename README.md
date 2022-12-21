ec163f83e482011d299ed5514e306d381e1ea41f
AylaCodes <Luckaylawoo@gmail.com>
2022-12-21 02:47:29 -0600

    Add rough draft simbrief integration

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Intended for use by livestreamers.
 
 ## Installation
 
-Simply copy the HTML/CSS/JS into a new script on Flow. I will provide a
-community-folder installable package at a later time.
+Simply copy the HTML/CSS/JS into a new script on Flow. and **replace {username} with your simbrief username** in the javascript file on line 27.
+
+I will provide a community-folder installable package at a later time once I'm happy
+with the state of the script.
 
 ## Example
 
-![example](https://uploads.wolfie.space/images/FlightSimulator_1671592539.png)
+![example](https://uploads.wolfie.space/images/FlightSimulator_1671611596.png)
--- overlay.js
+++ overlay.js
@@ -1,14 +1,50 @@
-let container = null;
 let get = this.$api.variables.get;
 
-// TODO: Fetch simbrief OFP
-// - ETE
-// - Distance
-// - REG/Callsign
+let container = null;
+
+let airframe = null;
+let registration = null;
+let origin = null;
+let destination = null;
+let airline = null;
 
 // TODO: Input network for vatsim/pe/etc
 
 // TODO: Plane input
+
+// TODO: Config colors
+
+// TODO: Config simbrief username
+
+run((event) => {
+    on_click = {
+        from: "wheel"
+    }
+
+    let req = new XMLHttpRequest();
+    req.open(
+        "GET",
+        "https://www.simbrief.com/api/xml.fetcher.php?username={username}&json=1"
+        );
+        req.responseType = "json";
+        req.send();
+        req.onload = () => {
+            let data = req.response;
+
+            airframe = data.aircraft.icaocode;
+            this.frame_label.innerText = `Type: ${airframe}`;
+            registration = data.aircraft.reg;
+            this.reg_label.innerText = registration;
+            origin = data.api_params.orig;
+            this.origin_label.innerText = `DEP: ${origin}`;
+            destination = data.api_params.dest.toUpperCase();
+            this.dest_label.innerText = `ARR: ${destination}`;
+            airline = data.api_params.airline;
+            this.airline_label.innerText = `Airline: ${airline}`;
+        }
+
+    return false;
+});
 
 loop_15hz(() => {
     let airspeed = get("A:AIRSPEED INDICATED", "number") * 1.944;
@@ -17,11 +53,11 @@ loop_15hz(() => {
     let altitude = get("A:PLANE ALTITUDE", "number") * 3.281;
     let heading = get("A:PLANE HEADING DEGREES MAGNETIC", "number") * (180/Math.PI);
 
-    this.as_label.innerText = `Indicated: ${Math.round(airspeed)} kts`;
-    this.gs_label.innerText = `Ground: ${Math.round(groundspeed)} kts`;
+    this.as_label.innerText = `IAS: ${Math.round(airspeed)} kts`;
+    this.gs_label.innerText = `G/S: ${Math.round(groundspeed)} kts`;
     this.vs_label.innerText = `V/S: ${Math.round(vs)} fpm`
-    this.alt_label.innerText = `Altitude: ${Math.round(altitude)} ft`;
-    this.hdg_label.innerText = `Heading: ${Math.round(heading)}`;
+    this.alt_label.innerText = `ALT: ${Math.round(altitude)} ft`;
+    this.hdg_label.innerText = `HDG: ${Math.round(heading)}`;
 });
 
 html_created((el) => {
@@ -31,4 +67,9 @@ html_created((el) => {
     this.vs_label = el.querySelector("#vs");
     this.alt_label = el.querySelector("#altitude");
     this.hdg_label = el.querySelector("#heading");
+    this.frame_label = el.querySelector("#airframe");
+    this.reg_label = el.querySelector("#registration");
+    this.origin_label = el.querySelector("#origin");
+    this.dest_label = el.querySelector("#destination");
+    this.airline_label = el.querySelector("#airline");
 });
