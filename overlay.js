0ca1a81367d4af50f1bb4c9a3ef7c7466e7e3107
AylaCodes <Luckaylawoo@gmail.com>
2023-05-09 04:01:25 -0500

    Add "dep" as as alias for "departure" search

--- overlay.js
+++ overlay.js
@@ -6,7 +6,7 @@ let ds_export = this.$api.datastore.export,
     ds_import = this.$api.datastore.import;
 
 // ---- Script variables
-const VERSION = "0.19.1";
+const VERSION = "0.19.2";
 
 const SIMBRIEF_URL = "https://www.simbrief.com/api/xml.fetcher.php?username=";
 
@@ -1004,6 +1004,7 @@ search(["overlay", "ol"], (query, callback) => {
         case "OG":
         case "DEPARTURE":
         case "DEPART":
+        case "DEP":
             if (params.length >= 3) {
                 results.push({
                     uid: "overlay_otto_15",
